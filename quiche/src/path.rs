94d0354304c65588516a68b8c8437afbde211a4d
Quentin De Coninck <quentin.deconinck@umons.ac.be>
2025-02-28 11:42:43 +0100

    cargo +nightly fmt

--- quiche/src/path.rs
+++ quiche/src/path.rs
@@ -1258,7 +1258,11 @@ impl PathMap {
 
     /// Are all paths in backup state?
     pub fn all_available_paths_backup(&self) -> bool {
-        !self.paths.iter().filter(|p| p.1.active()).any(|p| !p.1.is_backup())
+        !self
+            .paths
+            .iter()
+            .filter(|p| p.1.active())
+            .any(|p| !p.1.is_backup())
     }
 
     /// Handles the reception of PATH_BACKUP/PATH_AVAILABLE.
